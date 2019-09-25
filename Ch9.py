7b3a6dff65c38786cbb81a46f636ea36fd10e088
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-09-25 15:07:06 -0500

    Update Ch9.py

--- Ch9.py
+++ Ch9.py
@@ -32,3 +32,35 @@ words.close
 ##      2. the last 5 digits of n+1 are palindromic
 ##      3. the middle 4 digits of n+2 are palindromic
 ##      4. all 6 digits of n+3 are palindromic
+
+def odometer_test(num):
+  return (palindromic(str(num)[-4:]) and
+          palindromic(str(num+1)[-5:]) and
+          palindromic(str(num+2)[1:5]) and
+          palindromic(str(num+3)))
+
+def palindromic(s):
+  return s == s[::-1]
+
+for number in range(100000,999996):
+  if odometer_test(number):
+    print(number)
+    
+## Exercise 9.9 Write a Python program to determine age differences between a parent and child which yield the most palindromic ages.
+
+output = {}
+for i in range(15,45):                #age difference, assume minimum age of pregnancy of 15 and max of 45
+  for j in range(i,125):              #mother's age during palindrome test
+    temp = j - i 
+    if str(j).zfill(3) == str(temp)[::-1].zfill(3): #test for palindromicity
+      output[j] = temp
+
+i = {}
+for key,val in output.items():
+  j = key - val                       #age difference between key (mother age) & value (child age)
+  if j in i.keys():                   #total number of instances where that difference occurs
+    i[j] += 1
+  else:
+    i[j] = 1
+    
+print(i)
