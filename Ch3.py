81013f6cd274b9a5b32c86d5865c22ccefea8291
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2019-10-20 17:47:19 -0500

    add Exercises 3.1 & 3.2

--- Ch3.py
+++ Ch3.py
@@ -1,3 +1,9 @@
+##Exercise 3.1. Move the last line of this program to the top, so the function call appears before the definitions. Run the program and see what error message you get.
+# NameError: name 'repeat_lyrics' is not defined
+
+##Exercise 3.2. Move the function call back to the bottom and move the definition of print_lyrics after the definition of repeat_lyrics. What happens when you run this program?
+# Runs as normal
+
 ##Exercise 3.3. Write a function named right_justify that takes a string named s as a parameter and prints the string with enough leading spaces so that the last letter of the string is in column 70 of the display.
 
 def right_justify(s):
